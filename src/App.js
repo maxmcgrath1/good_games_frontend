93130973cf4882f26b34a1da5cbeace1e2c782e1
maxmcgrath1 <tuc06096@temple.edu>
2022-03-07 13:44:44 -0500

    added a navbar with links, updated app.js with routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from "react-router-dom"
 import Header from './components/Header';
@@ -10,7 +9,13 @@ import Games from './pages/Games';
 function App() {
   return (
     <div className="App">
-    
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/games"  element={<Games URL={URL} />} />
+        <Route path="/about"  element={<About URL={URL} />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
-const Header = () => {
+function Header(props) {
+    //inline style for the nav tag
+    const navStyle = {
+        display: "flex",
+        justifyContent: "space-around",
+        border: "3px solid black",
+        padding: "8px",
+        width: "90%",
+        margin: "auto",
+    };
+
     return (
-        <div>My Good Header</div>
-    )
+        <header>
+            <h1 className="maintitle">Welcome to the Games</h1>
+            <nav style={navStyle}>
+                <Link to="/">
+                    <div>HOME</div>
+                </Link>
+                <Link to="/games">
+                    <div>GAMES</div>
+                </Link>
+                <Link to="/about">
+                    <div>ABOUT/CONTACT</div>
+                </Link>
+            </nav>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
