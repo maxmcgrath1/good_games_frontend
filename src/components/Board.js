e21e187490d9413cb466729533f739c1c671b8ba
maxmcgrath1 <tuc06096@temple.edu>
2022-03-08 21:57:14 -0500

    Turns adn win conditions work, need draws and stops

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,6 +5,24 @@ import { useState } from "react";
 const Board = () => {
     const [squares, setSquares]=useState(Array(9).fill(null));
     const [player, setPlayer]=useState(true);
+    const winner = checkWin(squares);
+    
+    return (
+        <div class="gameContainer">
+            <div className="gameBoard">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+            <h2 className="gameMessage">{gameStatus()} </h2>
+        </div>    
+    );
 
     function renderSquare(i) {
         return <Square value={squares[i]} 
@@ -15,20 +33,38 @@ const Board = () => {
                     setPlayer(!player);
                 }} />;
     };
-    return (
-        <div className="gameBoard">
-            {renderSquare(0)}
-            {renderSquare(1)}
-            {renderSquare(2)}
-            {renderSquare(3)}
-            {renderSquare(4)}
-            {renderSquare(5)}
-            {renderSquare(6)}
-            {renderSquare(7)}
-            {renderSquare(8)}
-        </div>
-    )
+
+    function checkWin(squares) {
+        const conditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i=0; i < conditions.length; i++) {
+            const [a, b, c] = conditions[i];
+            if (squares[a] === squares[b] && squares[a] ===squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+    function gameStatus() {
+        if (winner) {
+            return "Game over, the winner is player " + winner + "!";
+        // } else if (checkDraw(squares)) {
+            return "This round is a draw!";
+        } else {
+            return "Player " + (player ? "X" : "O") + " is up, pick your square!"
+        }
+    }
 }
+
+
 
 export default Board;
 
--- src/components/Tictactoe.js
+++ src/components/Tictactoe.js
@@ -4,21 +4,12 @@ import { useState } from 'react';
 
 
 
-const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
+
 
 const Tictactoe = () => {
     return (
         <div>
-            Tic-Tac-Toe!
+            <h1>Tic-Tac-Toe!</h1>
             <Board />
         </div>
     )
