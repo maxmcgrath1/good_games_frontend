04327c815729d132ce485a6bd9098c35ec1c9741
maxmcgrath1 <tuc06096@temple.edu>
2022-03-13 22:25:10 -0400

    added delete button and method on show page--now at full CRUD

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router'
+import Button from 'react-bootstrap/esm/Button';
 
 function Show(props) {
     const {id} = useParams();
@@ -23,6 +24,14 @@ function Show(props) {
         })
         getGamesData();
     };
+
+    const deleteGame = async id => {
+        await fetch(props.URL + "games/" + id, {
+            method: "delete",
+            })
+        getGamesData()
+    };
+
     
     useEffect(() => getGamesData(), []);
 
@@ -38,12 +47,18 @@ function Show(props) {
     //     props.history.push("/")
     }
 
+    const removeGame = () => {
+        deleteGame(game._id)
+        // props.history.push("/")
+    }
+
     const loaded = () => {
         return (
             <div>
                 <h1>{game.name}</h1>
                 <img src={game.image} alt={game.name} />
                 <h2>{game.description}</h2>
+                <Button variant="danger" id="delete" onClick={removeGame}> DELETE GAME</Button>
                 <form onSubmit={handleSubmit}>
                 <input
                     type="text"
