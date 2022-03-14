f964b795ea3266bc320bc43344ce7e8ede02be87
maxmcgrath1 <tuc06096@temple.edu>
2022-03-14 19:54:09 -0400

    row of 3 function done

--- src/components/SquareSmash.js
+++ src/components/SquareSmash.js
@@ -25,6 +25,20 @@ const SquareSmash = () => {
         }
     };
 
+    const checkRowFour = () => {
+        for (let i = 0; i< 64; i++) {
+            const rowFour = [i, i + 1, i + 2]
+            const chosenColor = currentColorGroup[i]
+            const noCheck = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+
+            if (noCheck.includes(i)) continue
+
+            if (rowFour.every(square => currentColorGroup[square] === chosenColor)) {
+                rowFour.forEach(square => currentColorGroup[square] = '')
+            }
+        }
+    };
+
     const checkColumnThree = () => {
         for (let i = 0; i< 47; i++) {
             const columnThree = [i, i + width, i + width * 2]
@@ -32,6 +46,20 @@ const SquareSmash = () => {
 
             if (columnThree.every(square => currentColorGroup[square] === chosenColor)) {
                 columnThree.forEach(square => currentColorGroup[square] = '')
+            }
+        }
+    };
+
+    const checkRowThree = () => {
+        for (let i = 0; i< 64; i++) {
+            const rowThree = [i, i + 1, i + 2]
+            const chosenColor = currentColorGroup[i]
+            const noCheck = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+
+            if (noCheck.includes(i)) continue
+
+            if (rowThree.every(square => currentColorGroup[square] === chosenColor)) {
+                rowThree.forEach(square => currentColorGroup[square] = '')
             }
         }
     };
@@ -49,14 +77,15 @@ const SquareSmash = () => {
     
     useEffect(() => {
         const timer = setInterval(() => {
-            checkColumnFour()
-            checkColumnThree()
+            checkColumnFour();
+            checkColumnThree();
+            checkRowThree();
             setCurrentColorGroup([...currentColorGroup])
         }, 100)
         return () => clearInterval(timer)
-    }, [checkColumnFour, checkColumnThree, currentColorGroup])
+    }, [checkColumnFour, checkColumnThree, checkRowThree, currentColorGroup]);
 
-    console.log(currentColorGroup)
+    console.log(currentColorGroup);
 
     return (
         <div className='smashGameContainer'>
