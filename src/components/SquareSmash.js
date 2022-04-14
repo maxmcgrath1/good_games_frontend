2a8a8f8d17908ee08dbdb44f11c1286c997fa4da
maxmcgrath1 <tuc06096@temple.edu>
2022-04-14 16:42:23 -0400

    put timer comp in game

--- src/components/SquareSmash.js
+++ src/components/SquareSmash.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import SquareSmashGame from './SquareSmashGame'
+import Timer from './Timer'
 
 const SquareSmash = () => {
     return (
         <div className="smashGameBack">
         <h1 className='gameTitle'>SquareSmash!</h1>
         <SquareSmashGame />
+        <Timer />
     </div>
     )
 }
