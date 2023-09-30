817ed84db265438e88ec0283084649c0dee01f39
maxmcgrath1 <tuc06096@temple.edu>
2023-09-30 17:55:43 -0400

    fluff

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
-import reportWebVitals from './reportWebVitals';
-
 
 ReactDOM.render(
   <Router>
